8ea2ac200dc12005fef1f0611e8cd0533edbc32d
YuSoeun <doplinblue@handong.ac.kr>
2024-05-15 23:01:39 +0900

    24.05.15 최장증가부분수욜

---
+++ 11053_가장긴증가하는부분수열.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, temp, result = 0;
+    vector<int> A, B;
+    vector<int> matrix;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        A.push_back(temp);
+        matrix.push_back(0);
+    }
+
+    // 정렬된 것이랑 비교
+    int count, min = 0;
+    for (int i = N-1; i >= 0; i--) {
+        int max_cnt = 0;
+
+        for (int j = i; j < N; j++) {
+            if (A[j] > A[i]) {
+                if (matrix[j] > max_cnt)
+                    max_cnt = matrix[j];
+            }
+        }
+        matrix[i] = 1 + max_cnt;
+    }
+
+    for (int i : matrix) {
+        if (i > result)
+            result = i;
+    }
+
+    cout << result << "\n";
+
+    return 0;
+}
