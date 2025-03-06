8b67beeec9eb6cb4a0d683d5ad1f0c96cca354c0
doplinBlue <68419318+YuSoeun@users.noreply.github.com>
2025-03-07 02:27:23 +0900

    [Platinum II] Title: N-Queen 2, Time: 0 ms, Memory: 2156 KB, Score: 500 point -BaekjoonHub

--- 백준/Platinum/21133. N－Queen 2/N－Queen 2.cc
+++ 백준/Platinum/21133. N－Queen 2/N－Queen 2.cc
@@ -7,25 +7,30 @@ int main(int argc, char const *argv[]) {
     cin.tie(0);  ios::sync_with_stdio(0);
     cin >> N;
 
-    int num = 0;
-    // 예외: N을 6으로 나눴을 때 2또는 3
-    if (N % 6 == 2 || N % 6 == 3) {
-        for (int i = 0; i < N; i++) {
-            if (i < N/2) {
-                cout << 2*i + 2 << '\n';
-            } else {
-                cout << 2*(i-N/2) + 1 << '\n';
-            }
-        }
+    int t = N / 2;
+    vector<int> ans;
+
+    if (N % 6 == 2) {
+        for (int i = 1; i <= t; i++) ans.push_back(i * 2);
+        ans.push_back(3);
+        ans.push_back(1);
+        for (int i = 3; i < t; i++)  ans.push_back(i * 2 + 1);
+        ans.push_back(5);
+    } else if (N % 6 == 3) {
+        for (int i = 2; i <= t; i++) ans.push_back(i * 2);
+        ans.push_back(2);
+        for (int i = 2; i <= t; i++) ans.push_back(i * 2 + 1);
+        ans.push_back(1);
+        ans.push_back(3);
     } else {
-        if (N % 2 == 0) {
-            for (int i = 2; i <= N; i += 2)     cout << i << '\n';
-            for (int i = 1; i <= N-1; i += 2)   cout << i << '\n';
-        } else {
-            for (int i = 2; i <= N-1; i += 2)   cout << i << '\n';
-            for (int i = 1; i <= N; i += 2)     cout << i << '\n';
+        for (int i = 1; i <= t; i++) {
+            ans.push_back(i + t);
+            ans.push_back(i);
         }
+        if (N % 2 == 1) ans.push_back(N);
     }
+
+    for (int x : ans) cout << x << '\n';
 
     return 0;
 }
