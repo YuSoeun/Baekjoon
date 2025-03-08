02a76462eb61ff0db484a9a79d7ae041756e132a
doplinBlue <68419318+YuSoeun@users.noreply.github.com>
2025-03-08 21:56:11 +0900

    [Silver II] Title: 생태학, Time: 276 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <cmath>
+using namespace std;
+
+map <string, int> m;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+    string input;
+    
+    int cnt = 0;
+    while (getline(cin, input)) {
+        if (m.find(input) == m.end()) {
+            m[input] = 1;
+        } else {
+            m[input]++;
+        }
+        cnt++;
+    }
+
+    cout << fixed;
+    cout.precision(4);
+    for (auto e : m) {
+        cout << e.first << " " << (double)e.second/cnt*100 << "\n";
+    }
+
+    return 0;
+}
