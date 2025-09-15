f0917c98fc677309edcfd49edc3f33d055540f05
YuSoeun <doplinblue@handong.ac.kr>
2025-09-15 23:47:50 +0900

    feat: 트리의 지름 해결

--- Main_1967_diameterOfTree.java
+++ Main_1967_diameterOfTree.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main_diameterOfTree {
+public class Main_1967_diameterOfTree {
     static int N;
     static int result;
     static List<Node> tree[];
@@ -17,29 +17,37 @@ public class Main_diameterOfTree {
     }
 
     static int traverse(int from) {
-        int cost = 0;
-        int childCost = 0;
+        int child = 0;
+        int first = 0, second = 0;
 
         for (Node node : tree[from]) {
-            childCost = traverse(node.to);
-            cost = Math.max(childCost + cost, cost);
+            // System.out.println(from + " -> " + node.to + ": " + node.cost);
+            child = node.cost + traverse(node.to);
+
+            if (child > first) {
+                second = first;
+                first = child;
+            } else if (child > second) { 
+                second = child;
+            }
         }
 
-        result = Math.max(cost, result);
-        return cost;
+        result = Math.max(first + second, result);
+        // System.out.println("maxChild: " + maxChild + " / " + result);
+        return Math.max(first, second);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         tree = new ArrayList[N+1];
-        for (int i = 0; i <= N; i++) tree[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++)  tree[i] = new ArrayList<>();
         result = 0;
 
-        int from, to;
-        for (int i = 1; i <= N; i++) {
+        int from, to, cost;
+        for (int i = 1; i < N; i++) {
             st   = new StringTokenizer(br.readLine());
             from = Integer.parseInt(st.nextToken());
             to   = Integer.parseInt(st.nextToken());
