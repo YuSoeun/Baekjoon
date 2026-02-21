3292b2c4c1029f27f5916b7c7c1f8101e3039205
doplinBlue <68419318+YuSoeun@users.noreply.github.com>
2026-02-21 11:11:37 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,33 +1,19 @@
-
-
-
 -- 자동차 ID, 자동차 종류, 대여 금액(컬럼명: FEE) 리스트
 SELECT      CAR.CAR_ID
             , CAR.CAR_TYPE
-            # , HIS.START_DATE
-            # , HIS.END_DATE
-            , ROUND(CAR.DAILY_FEE * 30 -
-                (CAR.DAILY_FEE * 30 * PLAN.DISCOUNT_RATE / 100))
-                AS FEE
+            , ROUND(CAR.DAILY_FEE * 30 * (100 - PLAN.DISCOUNT_RATE) / 100) AS FEE
 FROM        CAR_RENTAL_COMPANY_CAR              CAR
-# JOIN        CAR_RENTAL_COMPANY_RENTAL_HISTORY   HIS
-#   ON        CAR.CAR_ID      = HIS.CAR_ID
 JOIN        CAR_RENTAL_COMPANY_DISCOUNT_PLAN    PLAN
   ON        CAR.CAR_TYPE    = PLAN.CAR_TYPE
   AND       PLAN.DURATION_TYPE = '30일 이상'
--- 1) 자동차 종류가 '세단' 또는 'SUV' 인 자동차 중
 WHERE       CAR.CAR_TYPE        IN ('세단', 'SUV')
--- 2) 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능하고
-  AND       CAR.CAR_ID NOT IN
-            (
-            SELECT  HIS_IN.CAR_ID
-            FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY HIS_IN
-            WHERE   START_DATE <= '2022-11-30'
+  AND       NOT EXISTS
+            (SELECT  1
+            FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY HIS
+            WHERE   HIS.CAR_ID = CAR.CAR_ID
+              AND   START_DATE <= '2022-11-30'
               AND   END_DATE >= '2022-11-01'
             )
---    30일간의 대여 금액이 50만원 이상 200만원 미만인 자동차
-  AND      (CAR.DAILY_FEE * 30 -
-                (CAR.DAILY_FEE * 30 * PLAN.DISCOUNT_RATE / 100)
-           ) BETWEEN 500000 AND 2000000
+  AND      (CAR.DAILY_FEE * 30 * (100 - PLAN.DISCOUNT_RATE) / 100) BETWEEN 500000 AND 2000000
 ORDER BY    FEE DESC, CAR_TYPE, CAR_ID DESC
 ;
