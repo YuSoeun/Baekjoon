ec7e62fe6da415e85bd3682a421a3c4251777814
doplinBlue <68419318+YuSoeun@users.noreply.github.com>
2026-03-03 21:03:56 +0900

    [Platinum II] Title: 방 청소, Time: 824 ms, Memory: 105924 KB -BaekjoonHub

---
+++ 백준/Platinum/9938. 방 청소/방 청소.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, L;
+    static int[] size;
+    static int[] p;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder   sb = new StringBuilder();
+        
+        // init
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        
+        size = new int[L+1];
+        p = new int[L+1];
+
+        Arrays.fill(size, 1);
+        for (int i = 1; i <= L; i++) {
+            p[i] = i;
+        }
+
+        // solution
+        int A, B;
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            if (B < A) {
+                int temp = A;
+                A = B;
+                B = temp;
+            }
+
+            union(A, B);
+            if (size[find(A)] > 0) {
+                size[find(A)]--;
+                sb.append("LADICA").append("\n");
+            } else {
+                sb.append("SMECE").append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa != pb) {
+            p[pb] = p[pa];
+            size[pa] += size[pb];
+        }
+    }
+
+    static int find(int x) {
+        if (x != p[x]) {
+            return p[x] = find(p[x]);
+        }
+        return x;
+    }
+}
